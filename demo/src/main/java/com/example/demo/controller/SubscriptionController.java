74da4d59a79fd8ef9ed0c0c81faba97c11e7d1fe
Stupendos <topakovdenis671@gmail.com>
2025-05-15 22:42:19 +0700

    API

--- demo/src/main/java/com/example/demo/controller/SubscriptionController.java
+++ demo/src/main/java/com/example/demo/controller/SubscriptionController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.SubsDTO;
 import com.example.demo.entity.Subscription;
 import com.example.demo.service.SubscriptionService;
 import org.springframework.http.HttpStatus;
@@ -27,9 +28,15 @@ public class SubscriptionController {
         return ResponseEntity.ok(subscriptionService.getUserSubscriptions(userId));
     }
 
-    @DeleteMapping("/{userId}")
-    public ResponseEntity<Void> deleteSubscription(@PathVariable Long subscriptionId) {
-        subscriptionService.deleteSubscription(subscriptionId);
+    @DeleteMapping("/{userId}/subscription/{subscriptionId}")
+    public ResponseEntity<Void> deleteSubscription(@PathVariable("userId") Long userId, @PathVariable Long subscriptionId) {
+        subscriptionService.deleteSubscription(userId, subscriptionId);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/top")
+    public ResponseEntity<List<SubsDTO>> getTopSubscriptions() {
+        List<SubsDTO> topSubs = subscriptionService.getTopSubs();
+        return ResponseEntity.ok(topSubs);
     }
 }
--- demo/src/main/java/com/example/demo/repository/SubscriptionRepository.java
+++ demo/src/main/java/com/example/demo/repository/SubscriptionRepository.java
@@ -1,11 +1,21 @@
 package com.example.demo.repository;
 
+import com.example.demo.dto.SubsDTO;
 import com.example.demo.entity.Subscription;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
     List<Subscription> findByUserId(Long userId);
+
+    Optional<Subscription> findByIdAndUserId(Long subscriptionId, Long userId);
+
+    @Query("SELECT s.subscriptionName, COUNT (s) as cnt FROM Subscription s GROUP BY s.subscriptionName ORDER BY cnt DESC ")
+    List<SubsDTO> findTopSubs(Pageable pageable);
 }
